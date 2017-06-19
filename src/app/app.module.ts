5970eb9bb77126f8acdf0b7a485c690ebdd910d9
Jason Ainsworth, Kai de Rochemont, Monique St. Laurent and Mohamed Warsame <jason@>
2017-06-19 16:58:42 -0700

    authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,49 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from "@angular/router";
+
+import {AngularFireModule} from "angularfire2";
 
 import { AppComponent } from './app.component';
+import { AuthService } from './providers/auth.service';
+import { LogInComponent } from './log-in/log-in.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRRH4wVXpbKRXfot4Qrvt21Olxvu_QKwE",
+  authDomain: "type-test-33c14.firebaseapp.com",
+  databaseURL: "https://type-test-33c14.firebaseio.com",
+  storageBucket: "type-test-33c14.appspot.com",
+  messagingSenderId: "826920843551"
+}
+
+const routes: Routes = [
+
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'login',
+    component: LogInComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "../providers/auth.service";
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
+
+}
