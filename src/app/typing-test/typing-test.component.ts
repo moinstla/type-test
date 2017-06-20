54a4a530816c47a6a746e6b98cbea6c5bd64cbd3
Kai <Guest@Epicodus-8E.local>
2017-06-20 14:17:45 -0700

    add CPM and time

--- src/app/typing-test/typing-test.component.ts
+++ src/app/typing-test/typing-test.component.ts
@@ -19,9 +19,12 @@ export class TypingTestComponent implements OnInit {
   progress: number = 0;
   successCounter: number = 0;
   capsLock: boolean;
-  time: number = 0;
+  startStamp;
+  endStamp;
   timerID;
   timerStatus: boolean = false;
+  roundTime: number;
+  roundCPM: number;
 
   constructor() { }
 
@@ -31,10 +34,27 @@ export class TypingTestComponent implements OnInit {
     this.displayArray = this.codeText.split('');
   }
 
+  startTime() {
+    if (this.timerStatus === false) {
+      this.timerStatus = true;
+      let d = new Date();
+      this.startStamp = d.getTime();
+    }
+  }
 
+  endTime() {
+    if (this.timerStatus === true) {
+      this.timerStatus = false;
+      let d = new Date();
+      this.endStamp = d.getTime();
+      this.roundTime = (this.endStamp - this.startStamp) / 1000;
+      this.roundCPM = (this.displayArray.length * 60) / this.roundTime;
+    }
+  }
 
   @HostListener('document:keypress', ['$event'])
   whatKey(event: KeyboardEvent) {
+    this.startTime();
     this.inputtedKey = event.key;
     this.totalKeys += 1;
       if (this.charsArray[this.successCounter] === this.inputtedKey) {
@@ -48,6 +68,8 @@ export class TypingTestComponent implements OnInit {
     this.capsLock = event.getModifierState("CapsLock");
     this.accuracy = (this.successArray.length / this.totalKeys) * 100;
     this.progress = (this.successArray.length / this.codeText.length) * 100;
+    if (this.progress === 100) {
+      this.endTime();
     }
-
+  }
 }
